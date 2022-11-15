6e5d5c43bd61849e43918e1c75f82028e7de9858
kleenex1 <curian48@naver.com>
2022-11-15 14:51:44 +0900

    #26 FEAT: 레시피 모델설정 및 MEDIA ROOT설정

--- cheers/models.py
+++ cheers/models.py
@@ -14,3 +14,16 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.email
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    image1 = models.ImageField(upload_to="recipe_pics")
+    image2 = models.ImageField(upload_to="recipe_pics", blank=True)
+    image3 = models.ImageField(upload_to="recipe_pics", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from cheers.views import CustomPasswordChangeView
-
 
 urlpatterns = [
     # admin
@@ -56,3 +57,5 @@ urlpatterns = [
 
     path('', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
