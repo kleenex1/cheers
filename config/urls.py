df36c4343f6fe4b2e6662f64df42a67fae6eaf1b
kleenex1 <curian48@naver.com>
2022-11-15 14:32:13 +0900

    #24 FEAT: 비밀번호 관련페이지 추가

--- config/urls.py
+++ config/urls.py
@@ -31,12 +31,28 @@ urlpatterns = [
     # 단순히 페이지를 render해주기만 하면되는 페이지라서 
     # 따로 view를 작성하지 않고 Generic view(template view)를 활용
     path("email-confirmation", 
-    TemplateView.as_view(template_name="cheers/email_confirmation.html") 
+    TemplateView.as_view(template_name="account/email_confirmation.html") 
     ,name="account_email_confirmation"),
+    path("password_change", 
+    TemplateView.as_view(template_name="account/password_change.html") 
+    ,name="password_change"),
+    path("password_reset_done", 
+    TemplateView.as_view(template_name="account/password_reset_done.html") 
+    ,name="password_reset_done"),
+    path("password_reset_from_key_done", 
+    TemplateView.as_view(template_name="account/password_reset_from_key_done.html") 
+    ,name="password_reset_from_key_done"),
+    path("password_reset_from_key", 
+    TemplateView.as_view(template_name="account/password_reset_from_key.html") 
+    ,name="password_reset_from_key"),
+    path("password_reset", 
+    TemplateView.as_view(template_name="account/password_reset.html") 
+    ,name="password_reset"),
     # Password 변경시 url 설정 (allauth가 설정한 곳이 아닌 커스텀하여 내가 redirect하고싶은 곳으로)
     # 'allauth.urls'보다 먼저 나와있어야 해당 url로 먼저 간다.
     path('password/change/', 
     CustomPasswordChangeView.as_view(), 
     name='account_password_change' ),
+
     path('', include('allauth.urls')),
 ]
