1f4ab4b47f4083c5501ed46836adec62197ed505
kleenex1 <curian48@naver.com>
2022-11-15 16:59:14 +0900

    #30 FEAT: Recipe View &  페이지

--- cheers/views.py
+++ cheers/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic import ListView
 from allauth.account.views import PasswordChangeView
+from cheers.models import Recipe
 # Create your views here.
 
-def index(request):
+class IndexView(ListView):
+    model = Recipe
+    template_name = 'cheers/index.html'
+    context_object_name = "recipes"
+    paginate_by = 4
+    ordering = ['-created_at']
     
-   
-    return render(request, "cheers/index.html")
 
 # 비밀번호 변경 페이지를 커스텀해주는 View 
 # 기존 PasswordChangeView를 상속받아서 자식 class에서 오버라이딩
