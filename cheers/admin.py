d8a914337d6427671bf8bffe6c70ba9c747478fb
kleenex1 <curian48@naver.com>
2022-11-16 14:47:45 +0900

    #75 FEAT: Model 추가(좋,댓,팔로우)

--- cheers/admin.py
+++ cheers/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Recipe
+from django.contrib.contenttypes.admin import GenericStackedInline
+from .models import User, Recipe, Comment, Like
 # Register your models here.
 
-admin.site.register(User, UserAdmin)
-# User모델의 추가 필드는 admin 페이지에 따로 나오지 않기 때문에 설정을 해줘야한다.
-UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname", "profile_pic", "introduce",)}),)
+class UserInline(admin.StackedInline):
+    model = User.following.through
+    fk_name = 'to_user'
+    verbose_name = 'Follower'
+    verbose_name_plural = 'Followers' 
 
-admin.site.register(Recipe)
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+# GenericForeignKey를 통해 관계를 맺고 있는것은 StackedInline을 못쓴다.
+# class LikeInline(admin.StackedInline):
+#     model = Like
+
+class LikeInline(GenericStackedInline):
+    model = Like
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+        LikeInline,
+    )
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        LikeInline,
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Like)
+
+
+# User모델의 추가 필드는 admin 페이지에 따로 나오지 않기 때문에 설정을 해줘야한다.
+UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname", "profile_pic", "introduce", "following")}),)
+UserAdmin.inlines = (UserInline,)
+
