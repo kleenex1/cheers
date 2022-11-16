97466f5e3fb88f6097333c278eef23ddc16e2cdc
kleenex1 <curian48@naver.com>
2022-11-16 16:45:43 +0900

    #85 FEAT: 댓글 수정,삭제 기능

--- cheers/urls.py
+++ cheers/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('update-profile/', views.ProfileUpdateView.as_view(), name='profile-update'),
     # 댓글
     path('recipes/<int:recipe_id>/comments/create/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:comment_id>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:comment_id>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+
 ]
